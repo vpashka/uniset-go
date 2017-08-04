9dba97b6a7812a4130c0ca656b22715b5286af27
Pavel Vaynerman <pv@etersoft.ru>
2017-08-04 03:46:33 +0300

    Изучаю go. Накидаваю первые прототипы необходимых структур и интерфейсов.

--- uniset.go
+++ uniset.go
@@ -1,30 +1,111 @@
+// Интерфейс для работы с uniset-датчиками
+// Основная идея:  подписка на уведомления об изменении датиков
+// Пользователь реализует интерфейс Consumer, предоставляющий канал
+// для посылки в него SensorMessage
+// Чтобы получать уведомления пользователь при помощи функции Subscribe
+// подписывается на события, указывая идентификатор желаемого датчика
 package uniset
 
 import (
 	"time"
+	"fmt"
+	"errors"
 )
 
+type SensorID int32
+type ObjectID int32
+
 type SensorMessage struct {
-	Value int32
+	Id        SensorID
+	Value     int32
 	Timestamp time.Time
 }
 
-type Consumer struct {
-	Name  string
-	MsgChannel <-chan SensorMessage
+func (m *SensorMessage) String() string {
+	return fmt.Sprintf("id: %d value: %d", m.Id, m.Value)
 }
+
+type Consumer interface {
+	EventChannel() chan SensorMessage
+	ID() ObjectID
+}
+
+type consumerList struct {
+	list []Consumer
+}
+
+func newConsumerList(sz int, cap int) (consumerList, error) {
+	lst := consumerList{}
+	return lst, nil
+}
+
+type askMap map[SensorID]consumerList
 
 type UInterface struct {
-	askmap map[string]Consumer
+	// map[sensorID] = consumer list
+	askmap askMap
 }
 
-func (ui *UInterface) ask(consumerID string, eventChannel <-chan SensorMessage) {
-	// \todo добавить проверку на существование заказчика
-	var c = Consumer{consumerID,eventChannel }
-	ui.askmap[consumerID] = c
+func NewUInterface() (UInterface, error) {
+	ui := UInterface{}
+	ui.askmap = make(askMap)
+	return ui, nil
 }
 
+func (ui *UInterface) Size() int {
+	return len(ui.askmap)
+}
 
-func MyFirstFunc() int {
-	return 42
+func (ui *UInterface) NumberOfCunsumers(sid SensorID) int {
+	v, found := ui.askmap[sid]
+	if found {
+		return len(v.list)
+	}
+
+	return 0
+}
+
+func (ui *UInterface) addConsumer(l *consumerList, cons Consumer) (err error) {
+
+	for _, c := range l.list {
+		if c.ID() == cons.ID() {
+			return nil
+		}
+	}
+
+	l.list = append(l.list, cons)
+	return nil
+}
+
+func (ui *UInterface) Subscribe(sid SensorID, cons Consumer) (err error) {
+	v, found := ui.askmap[sid]
+	if found {
+		ui.addConsumer(&v, cons)
+		return nil
+	}
+
+	lst, err := newConsumerList(1, 1)
+
+	if err != nil {
+		return err
+	}
+
+	ui.addConsumer(&lst, cons)
+	ui.askmap[sid] = lst
+	return nil
+}
+
+func (ui *UInterface) SendSensorMessage(sm SensorMessage) error {
+
+	v, found := ui.askmap[sm.Id]
+	if !found {
+		err := fmt.Sprintf("Not found: sensorID '%d'", sm.Id)
+		return errors.New(err)
+	}
+
+	for _, c := range v.list {
+		c.EventChannel() <- sm
+	}
+
+	return nil
 }
--- uniset_test.go
+++ uniset_test.go
@@ -3,13 +3,84 @@ package uniset_test
 import (
 	"testing"
 	"uniset"
+	"time"
 )
 
-func TestUniSet(t *testing.T) {
+// тестовый consumer
+type TestConsumer struct {
+	eventChannel chan uniset.SensorMessage
+	id           uniset.ObjectID
+	count	uint32
+}
 
-	var res = uniset.MyFirstFunc()
+func (c *TestConsumer) EventChannel() chan uniset.SensorMessage {
+	return c.eventChannel
+}
 
-	if res != 42 {
-		t.Error("Unknown result")
+func (c *TestConsumer) ID() uniset.ObjectID {
+	return c.id
+}
+
+func (c *TestConsumer) ReadMessage( t *testing.T, sid uniset.SensorID, count int, done chan<- bool) {
+
+	for i := 0; i < count; i++ {
+		msg := <-c.eventChannel
+		if msg.Id != sid {
+			t.Errorf("ReadMessage: sid=%d != %d", msg.Id, sid)
+		}
+		c.count++
+		//t.Log(msg.String())
 	}
+
+	done <- true
+}
+
+func TestSubscribe(t *testing.T) {
+
+	ui, err := uniset.NewUInterface()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	consumer := TestConsumer{make(chan uniset.SensorMessage, 10), 100, 0}
+
+	ui.Subscribe(10, &consumer)
+
+	if ui.Size() != 1 {
+		t.Errorf("Subscribe: size %d != %d",ui.Size(),1)
+	}
+
+	num := ui.NumberOfCunsumers(10)
+	if num != 1 {
+		t.Errorf("Subscribe: NumberOfCunsumers=%d != %d",num,1)
+	}
+
+	done := make(chan bool)
+
+	go consumer.ReadMessage(t, 10, 2, done)
+
+	sm1 := uniset.SensorMessage{10,10500, time.Now()}
+	sm2 := uniset.SensorMessage{11,100500, time.Now()}
+
+	err = ui.SendSensorMessage(sm1)
+	if err != nil {
+		t.Error("SendSensorMessage error: " + err.Error())
+	}
+
+	err = ui.SendSensorMessage(sm1)
+	if err != nil {
+		t.Error("SendSensorMessage error: " + err.Error())
+	}
+
+	err = ui.SendSensorMessage(sm2)
+	if err == nil {
+		t.Error("SendSensorMessage found Unknown sensor?!" + err.Error())
+	}
+
+	<-done
+
+	if consumer.count != 2 {
+		t.Errorf("Count of received messages %d != 2", consumer.count)
+	}
+
 }
